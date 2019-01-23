43f08bf7048ebe3edad128e847c13087b8dfd505
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-01-23 16:31:48 -0700

    Created knex set up

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('./knexfile.js')[env];
+const knex = require('knex')(config);
+
+// const sql = knex('methods').toString();
+
+// console.log(sql);
+
+// knex.destroy();
+
+app.get('/', (req, res) => {
+  // knex('methods')
+  knex('*').from('methods')
+    .then((rows) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      next(err);
+    });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ seeds/methods.js
@@ -0,0 +1,12 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('methods').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('methods').insert([
+        { id: 1, name: '.map' },
+        { id: 2, name: '.filter' },
+        { id: 3, name: '.reduce' }
+      ]);
+    });
+};
