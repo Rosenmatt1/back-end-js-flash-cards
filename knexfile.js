9f75b7a9e39f1dd658a009e971ca0b093b06d260
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-01-30 14:20:52 -0700

    did heroku addon and config

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,15 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://localhost/notecards'
+    connection:  'postgres://localhost/notecards'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+    
+    // "postgres://nfdtainkomvqtz:8402c295619e3c909994c399b656a1be00d88eb2648e547b8cd3292c4fa7bd8e@ec2-23-23-184-76.compute-1.amazonaws.com:5432/d7h1o8majnjrpn" 
   }
+
+
 };
